da982a47ae11b1ac449e1e940aeb6482342f66f9
DurgaArun88 <durgamzcet@gmail.com>
2025-01-23 23:31:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 import streamlit as st
 import pickle
 import numpy as np
+import gzip
 
-df=pickle.load(open('CreditCard.pkl','rb'))
-dtc=pickle.load(open('dtc.pkl','rb'))
+df=pickle.load(gzip.open('CreditCard.pkl.gz','rb'))
+dtc=pickle.load(gzip.open('dtc.pkl.gz','rb'))
 
 st.title("CREDIT CARD FRAUD DETECTION")
-input_data = st.text_input('Enter the input data')
-input_split_data = input_data.split('\t')
+st.write("This application predicts whether a transaction is valid or fraudulent based on the input data")
+
+input_data = st.text_input('Enter the input data(comma separated values)','')
+
 
 submit = st.button('Predict')
 
 if submit:
-  features = np.asarray(input_split_data, dtype=np.float64)
-  prediction = dtc.predict(features.reshape(1,-1))
+   try:
+      input_split_data = input_data.split(',')
+      features = np.asarray(input_split_data, dtype=np.float32)
+      prediction = dtc.predict(features.reshape(1,-1))
 
-  if prediction[0]==0:
-    st.write('Valid Transaction')
-  else:
-    st.write('Fraud Transaction')
+      if prediction[0]==0:
+        st.success('Valid Transaction')
+      else:
+        st.error('Fraud Transaction')
+
+   except ValueError:
+    st.error('Invalid input')
+   except Exception as e:
+    st.error(f'An error occurred: {e}')
